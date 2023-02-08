34d79f5546c95a75817d9a785e1ad930279382ca
c-annabel <106787550+c-annabel@users.noreply.github.com>
2023-02-09 00:08:55 +0100

    Delete demoproject directory

--- demoproject/myapp/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views   #dot (.) here indicates the same working directory as the file.
-
-urlpatterns = [
-    path('home/', views.home, name="home"), 
-    
-    #path('getuser/<name>/<id>', views.pathview, name='pathview'), 
-    #path('getuser/', views.qryview, name='qryview') 
-]
